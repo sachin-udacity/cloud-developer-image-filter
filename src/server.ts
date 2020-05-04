25969e839c02ea8f3eee00ca6f6d3ac64bcf1a11
ss-codie <39163189+ss-codie@users.noreply.github.com>
2020-05-03 21:37:57 -0400

    Updated message for base url

--- src/server.ts
+++ src/server.ts
@@ -62,7 +62,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send("Api supported: GET /filteredimage?image_url={param here}")
   } );
   
 
